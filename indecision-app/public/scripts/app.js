686b31f7d703f61373fe89647c6efc251c86fd95
Aga <a.kolia.grudzien@gmail.com>
2018-01-14 20:34:36 +0100

    first commit and set up babel

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('App.js is running');
+
+//JSX - JavaScript XML
+
+var template = React.createElement(
+  'p',
+  null,
+  'Does this change'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+//with render we provide 2 arguments: the first one is JSX, and the second one 
+//is the DOM element where we want to render it - to make it work in the browser 
+//we need to compile it into JS
