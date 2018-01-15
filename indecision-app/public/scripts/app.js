b440fd6b8a20396f0ba3347e5bde3de8260b2c0b
Aga <a.kolia.grudzien@gmail.com>
2018-01-15 21:26:35 +0100

    explore and learn JSX expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,68 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running');
 
 //JSX - JavaScript XML
 
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer"
+};
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change'
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+var user = {
+    name: "Andrew",
+    age: 26,
+    location: "New York"
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
@@ -15,3 +70,8 @@ ReactDOM.render(template, appRoot);
 //with render we provide 2 arguments: the first one is JSX, and the second one 
 //is the DOM element where we want to render it - to make it work in the browser 
 //we need to compile it into JS
+//working with JSX we can only have one root element that is why we wrap h1 an p into div element
+//for better visibility we may break the tags into line and wrap everything into the brackets
+//in curly brackets we provide JS expressions
+//we cannot refer only to an object as we gonna run into an error
+//but we may use object properties and will work fine
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,10 +2,42 @@ console.log('App.js is running');
 
 //JSX - JavaScript XML
 
-var template = <p>Does this change</p>;
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer"
+};
+var template = (
+<div>
+    <h1>{app.title}</h1> 
+    <p>{app.subtitle}</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+);
+
+var user = {
+    name: "Andrew",
+    age: 26,
+    location: "New York"
+};
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location }</p>
+    </div>
+)
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
 //with render we provide 2 arguments: the first one is JSX, and the second one 
 //is the DOM element where we want to render it - to make it work in the browser 
 //we need to compile it into JS
+//working with JSX we can only have one root element that is why we wrap h1 an p into div element
+//for better visibility we may break the tags into line and wrap everything into the brackets
+//in curly brackets we provide JS expressions
+//we cannot refer only to an object as we gonna run into an error
+//but we may use object properties and will work fine
