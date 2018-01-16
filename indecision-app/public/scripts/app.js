a0da000055a27963ad3a09cace0bbe167dc78ff9
Aga <a.kolia.grudzien@gmail.com>
2018-01-16 21:15:42 +0100

    render conditionals in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -6,7 +6,8 @@ console.log('App.js is running');
 
 var app = {
     title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer"
+    subtitle: "Put your life in the hands of a computer",
+    options: ["One", "Two"]
 };
 var template = React.createElement(
     "div",
@@ -16,10 +17,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         "p",
         null,
         app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "Here are your options" : "No options"
     ),
     React.createElement(
         "ol",
@@ -39,34 +45,40 @@ var template = React.createElement(
 
 var user = {
     name: "Andrew",
-    age: 26,
-    location: "New York"
+    age: 23,
+    location: "Memphis"
+
 };
 
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
 var templateTwo = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        user.name
+        user.name ? user.name : "Anonymous"
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         "p",
         null,
         "Age: ",
         user.age
     ),
-    React.createElement(
-        "p",
-        null,
-        "Location: ",
-        user.location
-    )
+    getLocation(user.location)
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 //with render we provide 2 arguments: the first one is JSX, and the second one 
 //is the DOM element where we want to render it - to make it work in the browser 
 //we need to compile it into JS
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,12 +4,14 @@ console.log('App.js is running');
 
 var app = {
     title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer"
+    subtitle: "Put your life in the hands of a computer",
+    options: ["One", "Two"]
 };
 var template = (
 <div>
     <h1>{app.title}</h1> 
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
         <li>Item one</li>
         <li>Item two</li>
@@ -19,20 +21,26 @@ var template = (
 
 var user = {
     name: "Andrew",
-    age: 26,
-    location: "New York"
+    age: 23,
+    location: "Memphis"
+    
 };
 
+function getLocation(location){
+    if (location) {
+        return <p>Location: {location}</p>;
+    } 
+}
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location }</p>
+        <h1>{user.name ? user.name : "Anonymous"}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 )
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 //with render we provide 2 arguments: the first one is JSX, and the second one 
 //is the DOM element where we want to render it - to make it work in the browser 
 //we need to compile it into JS
