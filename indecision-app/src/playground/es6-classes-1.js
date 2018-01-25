0d41a0c6d07e3c697e41db9075723746d6788c96
Aga <a.kolia.grudzien@gmail.com>
2018-01-25 18:47:52 +0100

    es6 subclasses revision

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -12,8 +12,44 @@ class Person {
     }
 }
 
-const me = new Person("Aga", 29);
-console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getDescription());
+
+//Subclasses
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);//we need to all parent constructor function
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    //we can override an existing parent method for Student creating a method with the same name:
+    getDescription () {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += `Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += ` I am visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+const me = new Traveller("Aga", 29, "Wrocław");
+console.log(me.getGreeting());
+
+const other = new Traveller();
+console.log(other.getGreeting());
