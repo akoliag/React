3f833f01dcd1f197a102b8ee549d21f726b761af
Aga <a.kolia.grudzien@gmail.com>
2018-01-26 21:53:40 +0100

    create and nest components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,64 +1,71 @@
-console.log('App.js is running');
-
-//JSX - JavaScript XML
-
-const app = {
-    title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer",
-    options: []
-};
-
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = " ";
-        render();
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Action/>
+                <Options/>
+                <AddOption/>
+            </div>
+        );
     }
-};
+}
 
-const onRemoveAll = () => {
-    app.options = [];
-    render();
-};
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision App</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
-const appRoot = document.getElementById('app');
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
 
-const numbers = [55, 101, 1000];
-const render = () => {
-    const template = (
+class Options extends React.Component {
+    render() {
+        return (
         <div>
-            <h1>{app.title}</h1> 
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                {
-                app.options.map((option)=> {
-                    return <li key={option}>{option}</li>;
-                })
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
+            <p>Options component here</p>
+            <Option/>
         </div>
-     );
-    ReactDOM.render(template, appRoot);
-};
+        );
+    }
+}
 
-render();
-//we just want to reference onFormSubmit function in the form - we don;t want to call it!
-//preventDefault when called, it's goinig to stop full page refresh 
-//and will allow us to run our function code
+class Option extends React.Component {
+    render(){
+        return (
+            <div>
+                Option component here
+            </div>
+        );
+    }
+}
+class AddOption extends React.Component {
+    render() {
+        return (
+        <div>
+            <p>AddOption component here</p>
+        </div>
+        );
+    }
+}
+//we extend the Header class by React.Component class and get access
+//to its all features
+//React components require a special method to be defined which is render
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById("app"));
+//we call React components using their names in html tags and they are reusable 
+//we can use them several times 
