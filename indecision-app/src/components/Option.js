d21630f1d3a90be61f0e0a6b761c93dd13fe6d31
Aga <a.kolia.grudzien@gmail.com>
2018-03-03 19:40:50 +0100

    refactoring stateless functional components

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-
-const Option = props => { //we can use export default with class in line but not with the variables
-  return (
-    <div>
-      {props.optionText}
-      <button
-        onClick={e => {
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+////we can use export default with class in line but not with the variables
+const Option = props => (
+  <div>
+    {props.optionText}
+    <button
+      onClick={e => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      Remove
+    </button>
+  </div>
+);
 
 export default Option; //we need to keep the name of the components when exporting so that the React has the names displayed in the browser
