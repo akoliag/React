95464289355b87dedc1e82437155e2e194d98c0c
Aga <a.kolia.grudzien@gmail.com>
2018-03-08 20:18:34 +0100

    add link between the routes

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';//BrowserRouter for creating a new router and Router for every single page
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';//BrowserRouter for creating a new router and Router for every single page
 
 
 const ExpenseDashboardPage = () => (
@@ -31,19 +31,34 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404! - <Link to="/">Go home</Link>
     </div>   
 );
 
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Get help</NavLink>
+    </header>   
+);
+//Header is shown on every page 
+
+//thanks to NavLink we use client side route not the server one - the page does not refresh to get the required data
 const routes = (
     <BrowserRouter>
-    <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/> 
-        <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
-        <Route component={NotFoundPage}/>
-    </Switch>
+    <div>
+        <Header />
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/> 
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+            <Route component={NotFoundPage}/>
+        </Switch>
+    </div>
     </BrowserRouter>
 );
 //exact will call only the page with exact route match
