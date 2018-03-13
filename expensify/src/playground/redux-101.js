003a94a6500aba81ff749881c47c25f0ee02c801
Aga <a.kolia.grudzien@gmail.com>
2018-03-13 18:49:47 +0100

    dispatch actions

--- expensify/src/playground/redux-101.js
+++ expensify/src/playground/redux-101.js
@@ -1,10 +1,54 @@
 import { createStore} from 'redux';
 
-const store = createStore((state = {count: 0 } ) => {
-    return state;
+
+const store = createStore((state = { count: 0 }, action ) => {
+    switch (action.type) {
+        case 'INCREMENT':
+        return {
+            count: state.count + 1
+        };
+        case 'DECREMENT':
+        return {
+            count: state.count - 1
+        };
+        case 'RESET':
+        return {
+            count: 0
+        };
+        default:
+            return state;
+    }
+    // if (action.type === 'INCREMENT') {
+    //     return {
+    //         count: state.count + 1
+    //     };
+    // } else {
+    //     return state;
+    // }
 });
 
 //store.getState(); returns the current state
-//createStore always requires a function as an argument: the first argument to this function is state (current state)
+//createStore always requires a function as an argument: the first argument to this function is state (current state) and the second is action
 
+console.log(store.getState());
+
+//Action  - an object that gets sent to the store
+
+//I'd like to increment the count 
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+//we call the dispatch method and name out action (by convention uppercased)
 console.log(store.getState());
