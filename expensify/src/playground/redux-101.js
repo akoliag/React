9488442ee8a4d05de378f8e446f87096a02ffa8b
Aga <a.kolia.grudzien@gmail.com>
2018-03-14 21:27:33 +0100

    subscribing and dynamic actions

--- expensify/src/playground/redux-101.js
+++ expensify/src/playground/redux-101.js
@@ -4,12 +4,14 @@ import { createStore} from 'redux';
 const store = createStore((state = { count: 0 }, action ) => {
     switch (action.type) {
         case 'INCREMENT':
+        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
         return {
-            count: state.count + 1
+            count: state.count + incrementBy
         };
         case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : -1;
         return {
-            count: state.count - 1
+            count: state.count - decrementBy
         };
         case 'RESET':
         return {
@@ -28,16 +30,22 @@ const store = createStore((state = { count: 0 }, action ) => {
 });
 
 //store.getState(); returns the current state
-//createStore always requires a function as an argument: the first argument to this function is state (current state) and the second is action
+//createStore always requires a function as an argument: the first argument to this function is state (current state) and the second is an action
 
+const unsubscribe = store.subscribe(() => {
+//gets called every single time the store changes
 console.log(store.getState());
+});
 
 //Action  - an object that gets sent to the store
 
 //I'd like to increment the count 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
+
+//unsubscribe();//if we call unsubscribe right here it will print out only 1 change the rest will not get called
 
 store.dispatch({
     type: 'INCREMENT'
@@ -50,5 +58,9 @@ store.dispatch({
 store.dispatch({
     type: 'DECREMENT'
 });
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 //we call the dispatch method and name out action (by convention uppercased)
-console.log(store.getState());
